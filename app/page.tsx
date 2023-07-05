9fca38224ad411dd4422e957160e2df0038ad50b
Aymeric FIGUIE <aymeric@kontestapp.com>
2023-07-05 10:57:27 +0200

    :sparkles: add some new components from shadcn for testing

--- app/page.tsx
+++ app/page.tsx
@@ -21,8 +21,40 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog"
+import {
+  HoverCard,
+  HoverCardContent,
+  HoverCardTrigger,
+} from "@/components/ui/hover-card"
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover"
+import { Label } from "@/components/ui/label"
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group"
+import { Skeleton } from "@/components/ui/skeleton"
+import { Input } from "@/components/ui/input"
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetDescription,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet"
 
-
+const SHEET_SIDES = ["top sheet", "right sheet", "bottom sheet", "left sheet"] as const
+ 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col p-24">
@@ -62,8 +94,83 @@ export default function Home() {
             </DialogContent>
           </Dialog>
         </div>
+        <div className="flex w-full justify-evenly">
+          <HoverCard>
+            <HoverCardTrigger>Hover Card</HoverCardTrigger>
+            <HoverCardContent   >
+              The React Framework – created and maintained by @vercel.
+            </HoverCardContent>
+          </HoverCard>
+          <Select>
+            <SelectTrigger className="w-[180px]">
+              <SelectValue placeholder="Select" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value="light">Light</SelectItem>
+              <SelectItem value="dark">Dark</SelectItem>
+              <SelectItem value="system">System</SelectItem>
+            </SelectContent>
+          </Select>
+        </div>
+        <div className="flex w-full justify-evenly">
+          <Popover>
+            <PopoverTrigger>Popover</PopoverTrigger>
+            <PopoverContent>Place content for the popover here.</PopoverContent>
+          </Popover>
+          <RadioGroup defaultValue="option-one">
+            <div className="flex items-center space-x-2">
+              <RadioGroupItem value="option-one" id="option-one" />
+              <Label htmlFor="option-one">Option One</Label>
+            </div>
+            <div className="flex items-center space-x-2">
+              <RadioGroupItem value="option-two" id="option-two" />
+              <Label htmlFor="option-two">Option Two</Label>
+            </div>
+          </RadioGroup>
+        </div>
+        <div className="flex w-full justify-evenly">
+          <div>
+            <Skeleton className="w-[100px] h-[20px] rounded-full mb-2" />
+            <Skeleton className="w-[80px] h-[20px] rounded-full" />
+          </div>
+          <div className="grid grid-cols-2 gap-2">
+            {SHEET_SIDES.map((side) => (
+              <Sheet key={side}>
+                <SheetTrigger asChild>
+                  <Button variant="outline">{side}</Button>
+                </SheetTrigger>
+                <SheetContent side={side}>
+                  <SheetHeader>
+                    <SheetTitle>Edit profile</SheetTitle>
+                    <SheetDescription>
+                      Make changes to your profile here. Click save when you're done.
+                    </SheetDescription>
+                  </SheetHeader>
+                  <div className="grid gap-4 py-4">
+                    <div className="grid grid-cols-4 items-center gap-4">
+                      <Label htmlFor="name" className="text-right">
+                        Name
+                      </Label>
+                      <Input id="name" value="Pedro Duarte" className="col-span-3" />
+                    </div>
+                    <div className="grid grid-cols-4 items-center gap-4">
+                      <Label htmlFor="username" className="text-right">
+                        Username
+                      </Label>
+                      <Input id="username" value="@peduarte" className="col-span-3" />
+                    </div>
+                  </div>
+                  <SheetFooter>
+                    <SheetClose asChild>
+                      <Button type="submit">Save changes</Button>
+                    </SheetClose>
+                  </SheetFooter>
+                </SheetContent>
+              </Sheet>
+            ))}
+          </div>
+        </div>
       </div>
-      
     </main>
   )
 }
